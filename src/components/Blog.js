0b1e102b6b496d0fbc322369d5edfc26ecbe5267
absurd-turtle <absurd.turtl3@gmail.com>
2022-07-07 15:25:37 +0300

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author.name}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [viewExtraInfo, setViewExtraInfo] = useState(false)
+  
+  const toggleExtraView = () => {
+    setViewExtraInfo(!viewExtraInfo)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author.name}
+      <button onClick={toggleExtraView}>{viewExtraInfo ? "hide" : "view"}</button>
+      {
+        viewExtraInfo && <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
